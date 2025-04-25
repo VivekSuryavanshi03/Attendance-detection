b6ed9228f800c868305da33260ffdb8ccd95b87c
VivekSuryavanshi03 <vivek.2022ug3014@iiitranchi.ac.in>
2025-04-26 01:45:11 +0530

    f

---
+++ install_cmake.sh
@@ -0,0 +1,13 @@
+[1mdiff --git a/install_cmake.sh b/install_cmake.sh[m
+[1mindex aa81d52..26f2cb4 100644[m
+[1m--- a/install_cmake.sh[m
+[1m+++ b/install_cmake.sh[m
+[36m@@ -1,8 +1,4 @@[m
+ #!/bin/bash[m
+ echo "Installing CMake..."[m
+ apt-get update[m
+[31m-<<<<<<< HEAD[m
+ apt-get install -y cmake build-essential[m
+[31m-=======[m
+[31m-apt-get install -y cmake build-essential[m
+[31m->>>>>>> befd269f41fd24c64c4ff8f823d3facfd386fc73[m
