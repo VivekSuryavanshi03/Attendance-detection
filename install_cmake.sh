3960d7e3a4a3789c4e6fdeec250653748ecb70d8
VivekSuryavanshi03 <vivek.2022ug3014@iiitranchi.ac.in>
2025-04-26 01:43:34 +0530

    Updated install_cmake.sh

--- install_cmake.sh
+++ install_cmake.sh
@@ -1,8 +1,4 @@
 #!/bin/bash
 echo "Installing CMake..."
 apt-get update
-<<<<<<< HEAD
 apt-get install -y cmake build-essential
-=======
-apt-get install -y cmake build-essential
->>>>>>> befd269f41fd24c64c4ff8f823d3facfd386fc73
